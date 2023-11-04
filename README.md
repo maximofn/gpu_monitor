1a7262e2edf7553375cfe2aedf2501d848a9144a
maximofn <maximofn@gmail.com>
2023-11-04 23:34:16 +0100

    First version of gpu monitor

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# gpu_monitor
-Monitor of GPU for menu bar of Ubuntu
+# GPU Monitor
+
+Real-time GPU status tracking right on your Ubuntu menu bar.
+
+![gpu monitor](https://maximofn.com/wp-content/uploads/2023/11/gpu_monitor.png)
+
+## About GPU Monitor
+GPU Monitor is an intuitive tool designed for developers, gamers, and professionals who need to keep an eye on their graphics card's performance and health in real time. It integrates seamlessly with the Ubuntu menu bar, providing essential information at your fingertips.
+
+## Key Features
+ * Real-time Monitoring: View GPU utilization, memory and temperature, all updated live.
+ * Multi-GPU Support: Manage and monitor multiple GPUs from a single instance.
+ * Optimized for Ubuntu: Crafted to integrate flawlessly with the latest Ubuntu OS.
+
+## Installation
+
+### Clone the repository
+
+```bash
+git clone https://github.com/maximofn/gpu_monitor.git
+```
+
+or with `ssh`
+
+```bash
+git clone git@github.com:maximofn/gpu_monitor.git
+```
+
+### Install the dependencies
+
+Make sure that you do not have any `venv` or `conda` environment installed.
+
+```bash
+if [ -n "$VIRTUAL_ENV" ]; then
+    deactivate
+fi
+if command -v conda &>/dev/null; then
+    conda deactivate
+fi
+deactivate
+```
+
+Now install the dependencies
+
+```bash
+sudo apt-get install python3-gi python3-gi-cairo gir1.2-gtk-3.0
+sudo apt-get install gir1.2-appindicator3-0.1
+pip install nvidia-ml-py3
+pip install pynvml
+```
+
+## Execution at start-up
+
+Opens the application menu
+
+![application menu](https://maximofn.com/wp-content/uploads/2023/11/applications_menu.png)
+
+Type "start"
+
+![start](https://maximofn.com/wp-content/uploads/2023/11/applications_at_startup.png)
+
+Click on "Startup Applications"
+
+![startup applications](https://maximofn.com/wp-content/uploads/2023/11/startup-application.webp)
+
+Select "Add" and write the following:
+
+ * Name: GPU Monitor
+ * Command: /usr/bin/python3 path_of_script/gpu_monitor.py
+ * Comment: GPU Monitor
+
+And click on "Add"
+
+## Support
+
+Consider giving a **☆ Star** to this repository, if you also want to invite me for a coffee, click on the following button
+
+[![BuyMeACoffee](https://img.shields.io/badge/Buy_Me_A_Coffee-support_my_work-FFDD00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=white&labelColor=101010)](https://www.buymeacoffee.com/maximofn)
---
+++ gpu_monitor.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import signal
+import gi
+gi.require_version('AppIndicator3', '0.1')
+from gi.repository import AppIndicator3, GLib
+from gi.repository import Gtk as gtk
+import os
+import pynvml
+
+APPINDICATOR_ID = 'GPU_monitor'
+
+def main():
+    path = os.path.dirname(os.path.realpath(__file__))
+    icon_path = os.path.abspath(f"{path}/tarjeta-de-video.png")
+    GPU_indicator = AppIndicator3.Indicator.new(APPINDICATOR_ID, icon_path, AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
+    GPU_indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+    GPU_indicator.set_menu(build_menu())
+
+    # Get GPU info
+    device_count, gpu_info = get_gpu_info()
+
+    info = ""
+    for i in range(device_count):
+        info += f" {i}->"
+        if device_count > 1:
+            info += f"{int(gpu_info[i]['memory_used'])}/"
+            info += f"{int(gpu_info[i]['memory_total'])}MB->"
+            info += f"{int(gpu_info[i]['temp'])}ºC"
+        else:
+            info += f"{gpu_info[i]['memory_used']}/"
+            info += f"{gpu_info[i]['memory_total']}MB->"
+            info += f"{gpu_info[i]['temp']}ºC"
+
+    GPU_indicator.set_label(info, "Indicator")
+
+    GLib.MainLoop().run()
+
+def build_menu():
+    menu = gtk.Menu()
+    item_hello = gtk.MenuItem(label='Hola Mundo')
+    # item_hello.connect('activate', hello)
+    menu.append(item_hello)
+    # menu.show_all()
+    return menu
+
+def get_gpu_info():
+    # Init NVML
+    pynvml.nvmlInit()
+
+    # Get number of devices
+    device_count = pynvml.nvmlDeviceGetCount()
+
+    gpu_info = {}
+
+    for i in range(device_count):
+        gpu_info[i] = {}
+
+        # Obtener el identificador del dispositivo
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        
+        # Obtener la información de memoria
+        memory_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        
+        # Obtener la información de la temperatura
+        temp = pynvml.nvmlDeviceGetTemperature(handle, pynvml.NVML_TEMPERATURE_GPU)
+
+        memory_used = memory_info.used / 1024**2
+        memory_total = memory_info.total / 1024**2
+
+        gpu_info[i]["memory_used"] = memory_used
+        gpu_info[i]["memory_total"] = memory_total
+        gpu_info[i]["temp"] = temp
+        
+    # Finalizar NVML
+    pynvml.nvmlShutdown()
+
+    return device_count, gpu_info
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL) # Allow the program to be terminated with Ctrl+C
+    main()
