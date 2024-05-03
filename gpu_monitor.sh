ef42022d3533c0bbc12f73c53f4503bdfd1fa910
maximofn <maximofn@gmail.com>
2024-05-03 09:17:47 +0200

    Change folder of logs

--- gpu_monitor.sh
+++ gpu_monitor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Get script path
 SCRIPT_PATH=$(dirname $0)
-/usr/bin/python3 $SCRIPT_PATH/gpu_monitor.py >/home/wallabot/Escritorio/gpu_monitor.log 2>/home/wallabot/Escritorio/gpu_monitor_error.log
+/usr/bin/python3 $SCRIPT_PATH/gpu_monitor.py >gpu_monitor.log 2>gpu_monitor_error.log
